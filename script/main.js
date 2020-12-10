7822c3bf0794a7c05a31fba093d635d60968339a
sepehr-eskini <sepehr.eskini@gmail.com>
2020-12-10 15:23:41 +0330

    Making side menu a little beter but not completely

--- script/main.js
+++ script/main.js
@@ -4,20 +4,14 @@ var mainContainer = document.getElementsByClassName("main-container")[0];
 var sideMenuContainer = document.getElementsByClassName("side-menu-container")[0];
 var sideMenuCancelButton = document.getElementById("side-menu-cancel-button");
 var sideMenuOpened = false;
-let currentWidth = 0;
 sideMenuButton.onclick = function() {
     if(sideMenuOpened) {
-        body.style.marginLeft = "0px";
-        //mainContainer.className = "main-container";
+        mainContainer.className = "main-container";
         sideMenuContainer.classList.remove("open-menu");
         sideMenuCancelButton.style.width = "0px";
         sideMenuOpened = false;
     } else {
-        currentWidth = mainContainer.offsetWidth;
-        mainContainer.style.position = "fixed";
-        mainContainer.style.width = currentWidth;
-        mainContainer.style.marginLeft = "270px";
-        //mainContainer.className = "main-container-open-side";
+        mainContainer.className = "main-container-open-side";
         sideMenuContainer.classList.add("open-menu");
         sideMenuCancelButton.style.width = "calc(100% - 270px)";
         sideMenuOpened = true;
